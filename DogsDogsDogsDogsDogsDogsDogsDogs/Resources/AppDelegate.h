6c2848521a3339dc001e98a2210cc5916abb87da
Nic Gibson <nic@shpangy.com>
2019-07-03 14:49:26 -0600

    Initial Commit

---
+++ DogsDogsDogsDogsDogsDogsDogsDogs/Resources/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DogsDogsDogsDogsDogsDogsDogsDogs
+//
+//  Created by Nic Gibson on 7/3/19.
+//  Copyright © 2019 Nic Gibson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
